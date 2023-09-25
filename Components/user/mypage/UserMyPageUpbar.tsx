51dd3a9cdb008b9badcfb8e18abaddb344c97e83
JangGwon <tlsakch510@naver.com>
2023-09-25 17:44:44 +0900

    feat : 유저 마이페이지 - 옵션 바 UI구현

---
+++ Components/user/mypage/UserMyPageUpbar.tsx
@@ -0,0 +1,19 @@
+
+// <div className="border-4 border-black w-1 h-8 my-auto"></div>
+type UserMyPageUpbartype = {
+    setOption: React.Dispatch<React.SetStateAction<number>>
+}
+
+export default function UserMyPageUpbar({ setOption }: UserMyPageUpbartype) {
+
+    return (
+        <div className="flex font-semibold font-sm gap-8 w-[70rem] h-16 bg-white pl-24">
+            <button onClick={() => setOption(1)}>좋아요 표시한 프로젝트</button>
+            <button onClick={() => setOption(2)}>최근에 본 프로젝트</button>
+            <button onClick={() => setOption(3)}>팔로우 한 크리에이터</button>
+            <button onClick={() => setOption(4)}>개발노트 - 탭버튼(임시)</button>
+        </div>
+    );
+
+}
+
--- pages/usermypage.tsx
+++ pages/usermypage.tsx
@@ -1,7 +1,15 @@
 import UserMyPageProfile from "@/Components/user/mypage/UserMyPageProfile";
+import UserMyPageUpbar from "@/Components/user/mypage/userMyPageUpbar";
+import { useState } from "react";
+
 
 export default function Usermypage() {
+    const [selectedOption, setSelectedOption] = useState<number>(1);
+
     return (<div>
-        <UserMyPageProfile />
+        <div className="flex justify-center gap-4 my-6">
+            <UserMyPageProfile />
+            <UserMyPageUpbar setOption={setSelectedOption} />
+        </div>
     </div>);
 }

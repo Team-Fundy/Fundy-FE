13619fbbff6ac7df4864a4bef8e4ea9d406a00cf
JangGwon <tlsakch510@naver.com>
2023-10-23 00:23:33 +0900

    style : 신규프로젝트 컴포넌트 상단 메뉴 & 메세지 디자인

--- Components/Home/NewProject/NewProjectComponent.tsx
+++ Components/Home/NewProject/NewProjectComponent.tsx
@@ -7,16 +7,17 @@ export default function NewProjectListComponent() {
     const projectlist = [null, null, null, null, null];
     return (
         <div className="bg-gray-200">
-            <div className="h-24 flex justify-between">
-                <div className="flex place-items-center">
-                    <Image src={newprojecticon} alt="none" className="h-12 " />
-                    <p className="my-3 bold font-['dalmoori']">신규프로젝트 </p>
+            <div className="h-18 flex justify-between mb-2">
+                <div className="flex gap-4">
+                    <Image src={newprojecticon} alt="none" className="h-12 my-2" />
+                    <p className="my-3 bold text-3xl font-bold">신규프로젝트 </p>
                 </div>
-                <button>
+                <button className="h-4">
                     <Image src={entirebtn} alt="전체보기" />
                 </button>
             </div>
-            <div className="my-16">
+            <div className="mb-6 text-slate-400">
+                <p>장르별 프로젝트는 장르를 선택하여 머시깽이 하는 머시깽이 입니다.</p>
             </div>
             <NewProjectList />
             <div className="h-12">

147e50c0bcd81de955841ffbde6a67bdd237e851
JangGwon <tlsakch510@naver.com>
2023-11-28 00:20:19 +0900

    redesign : 메인홈 - 개발노트컴포넌트 아이템 재디자인

--- Components/Home/DevelopNote/DevelopNoteItem.tsx
+++ Components/Home/DevelopNote/DevelopNoteItem.tsx
@@ -1,20 +1,23 @@
 import board from "@/public/home/developnote-title-bg.png";
 import tempimg from "@/public/home/temp/image 39.png";
+import footerImg from   "@/public/home/developnote-title-footer.png";
 import Image from "next/image";
 
 export default function DevelopNoteItem() {
     const title = "던전에 어서와";
     const developNote_weekNum = "1";
     return (
-        <div>
-            <button className=" w-64 relative z-0 overflow-hidden">
-                <Image src={board} alt="" className="w-full h-full object-cover" />
-                <Image src={tempimg} alt="" className="relative z-1 bottom-[20rem] left-[5.5rem] w-24 h-32 border-2 border-white" />
-                <div className="absolute bottom-[13rem] left-[1rem]">
-                    <p className="text-2xl font-bold text-white">{title}</p>
-                    <p className="bg-[#583B6A] text-[#D0CCFF] font-semibold text-sm text-left px-2 py-1 my-2 w-fit rounded">{developNote_weekNum}주차 개발노트</p>
+        <div className ="relative z-0">
+ 
+            <button className="w-52 relative z-2">
+                <Image src={board} alt="" className="w-52 object-cover" />
+                <Image src={tempimg} alt="" className="absolute z-1 bottom-[6.5rem] left-[4.3rem] w-20 h-28 border-1 border-white" />
+                <div className="absolute bottom-[1.5rem] left-[1.5rem]">
+                    <p className="text-lg font-bold text-white">{title}</p>
+                    <p className="bg-[#583B6A] text-[#D0CCFF] font-semibold text-[0.5rem] text-left px-2 py-1 w-fit rounded">{developNote_weekNum}주차 개발노트</p>
                 </div>
             </button>
+            <Image src={footerImg} alt ="none" className ="absolute z-0 bottom-1"/>
         </div>
     );
 }

6e2beb6a7eef9cd3b2f8c1941e5db4d44309b2f7
JangGwon <tlsakch510@naver.com>
2023-11-27 18:39:40 +0900

    restyle : 메인홈 - 개발노트컴포넌트 헤더 재디자인

--- Components/Home/DevelopNote/DevelopNoteComponent.tsx
+++ Components/Home/DevelopNote/DevelopNoteComponent.tsx
@@ -1,5 +1,5 @@
 import developicon from "@/public/home/icon/developIcon.png"
-import entirebtn from "@/public/home/button/entirebutton.png"
+import entirebtn from "@/public/home/button/entirebutton2.png"
 import unDirBtn from "@/public/home/button/leftDirectionWhiteButton.png"
 import onDirBtn from "@/public/home/button/rightDirectionBlackButton.png"
 import Image from "next/image";
@@ -28,15 +28,15 @@ export default function DevelopNoteComponent() {
                     <div className="w-full flex gap-4 my-6">
                         <Image src={developicon} alt="none" className="h-12" />
                         <p className="my-2 font-bold text-2xl">개발노트 </p>
-                        <p className="my-3 ml-8 text-slate-300">장르별 프로젝트는 장르를 선택하여 머시깽이 하는 머시깽이 입니다.</p>
+                        <p className="my-3 ml-2 text-slate-300 text-sm">장르별 프로젝트는 장르를 선택하여 머시깽이 하는 머시깽이 입니다.</p>
                     </div>
-                    <div className ="flex">
+                    <div className ="flex gap-4">
                     <button>
-                        <Image src={entirebtn} alt="전체보기" />
+                        <Image src={entirebtn} alt="전체보기" className ="w-32 h-8" />
                     </button>
                     <button className ="flex my-auto gap-2">
-                        <Image src={unDirBtn} alt="leftbtn"></Image>
-                        <Image src={onDirBtn} alt="rightbtn"></Image>
+                        <Image src={unDirBtn} alt="leftbtn" className ="w-12 h-12"></Image>
+                        <Image src={onDirBtn} alt="rightbtn" className ="w-12 h-12"></Image>
                     </button>
                     </div>
                 </div>

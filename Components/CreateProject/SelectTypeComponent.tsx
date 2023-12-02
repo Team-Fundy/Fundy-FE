5fda0e87c13fa4b5c9c80bf01e15490de73aac41
JangGwon <tlsakch510@naver.com>
2023-12-02 22:24:03 +0900

    feat : 프로젝트 업로드페이지< 프로젝트 종류 선택창> - 프로젝트 종류 클릭안하고 다음버튼 눌렀을때 경고창 구현

--- Components/CreateProject/SelectTypeComponent.tsx
+++ Components/CreateProject/SelectTypeComponent.tsx
@@ -12,6 +12,15 @@ export default function SelectProjectTypeComponent()
 {
     const [selectedProjectType,setSelectedProjectType] = useState<string>("null")
 
+    function onClickNextBtn()
+    {
+        if(selectedProjectType === "null")
+        {
+            alert("프로젝트 종류를 선택해주세요");
+            return ;
+        }
+    }
+
     return (
         <div className = "w-3/5 mx-auto my-8">
             <p className ="text-lg font-semibold my-2">프로젝트 선택하기</p>
@@ -30,7 +39,7 @@ export default function SelectProjectTypeComponent()
                     <Image src={leftdirBtn} alt ="leftButton" className ="w-3 h-3 my-auto"/>
                     <p className ="my-auto text-gray-500 text-sm">뒤로가기</p>
                  </Link>
-                <button className ="w-44 h-10 bg-white">
+                <button className ="w-44 h-10 bg-white" onClick={onClickNextBtn}>
                     다음으로 
                 </button>
             </div>

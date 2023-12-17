eb6b8b9e5e4e7a2582c6d77ee5758fb2c2fea372
JangGwon <tlsakch510@naver.com>
2023-12-17 23:43:49 +0900

    design : 프로젝트 업로드 페이지 - 크리에이터 정보 입력Form - 크리에이터 소개글 작성 Input 디자인

--- Components/CreateProject/CreateProjectForm/CreatorInformFrom.tsx
+++ Components/CreateProject/CreateProjectForm/CreatorInformFrom.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useState } from "react"
 export default function CreatorInformFrom()
 {
     const [creatorName, setCreatorName] = useState<string>("");
+    const [creatorIntroduce, setCreatorIntroduce] = useState<string>("");
 
     function getInputBorderColor(str : string, lengthLimit : number)
     {
@@ -40,10 +41,10 @@ export default function CreatorInformFrom()
                     <h2 className ="font-semibold text-lg my-2">크리에이터 이름</h2>
                     <p className ="my-auto text-xl text-red-500">*</p>
                 </div>
-                <p className ="my-2">크리에이터 혹은 크리에티 팀의 이름을 작성해주세요.</p>
+                <p className ="my-2 text-gray-600">크리에이터 혹은 크리에티 팀의 이름을 작성해주세요.</p>
                 <input 
-                    className={`rounded-sm w-96 h-12 border-[0.1rem] ${getInputBorderColor(creatorName,40)}`}
-                    placeholder="     40자 이내의 이름을 작성해주세요."
+                    className={`rounded-sm w-96 h-12 border-[0.1rem] ${getInputBorderColor(creatorName,40)} text-start`}
+                    placeholder="   40자 이내의 이름을 작성해주세요."
                     onChange={(event)=>setCreatorName(event.target.value)}/>
                 <div className ="flex justify-between">
                     { creatorName.length === 0
@@ -73,9 +74,19 @@ export default function CreatorInformFrom()
 
             <br/>
 
-            <h2>크리에이터 소개 글</h2>
-            <p>크리에이터 혹은 크리에티 팀의 소개글을 작성해주세요.</p>
-            <input placeholder="150자 이내의 설명을 작성해주세요."/>
+            <div className ="w-[34rem] my-14">
+                <h2 className ="font-semibold text-lg my-3">크리에이터 소개 글</h2>
+                <p className ="my-2 text-gray-600">크리에이터 혹은 크리에티 팀의 소개 글을 작성해주세요..</p>
+                <input 
+                    className={`rounded-sm w-[34rem] h-[7.5rem] border-[0.1rem] border-gray-500 text-start`}
+                    placeholder=" 150자 이내의 이름을 작성해주세요."
+                    onChange={(event)=>setCreatorIntroduce(event.target.value)}/>
+                <div className ="flex justify-between">
+                    <p></p>
+                    <p className={`my-2 text-sm ${getInputLimitStringColor(creatorIntroduce,150)} `}>{getInputStringLength(creatorIntroduce)}/150자</p>
+                </div>
+            </div>
+
 
             <br/>
 

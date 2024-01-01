ed74f42f1f030935ea69c246d91f295c0403e54f
JangGwon <tlsakch510@naver.com>
2024-01-02 02:13:17 +0900

    feat : 프로젝트 업로드 페이지 : 동영상 추가 모달 구현

--- Components/CreateProject/CreateProjectForm/ProjectionInformForm/UploadImageList.tsx
+++ Components/CreateProject/CreateProjectForm/ProjectionInformForm/UploadImageList.tsx
@@ -19,7 +19,8 @@ type UploadImageList = {
 
 
 export default function UploadImageList({imageList, setImageList} :UploadImageList ){
-    //const []
+    
+    const [isVideoInputModalOpen, setIsVideoInputModalOpen] = useState(false);
 
     function onClickAddImageBtn()
     {
@@ -33,8 +34,13 @@ export default function UploadImageList({imageList, setImageList} :UploadImageLi
         fileInput.click();
     }
 
+    function onClickAddVideoBtn()
+    {
+        setIsVideoInputModalOpen(true);
+    }
+
     return (
-    <div> 
+    <div className ="relative"> 
         <h2 className ="font-semibold text-lg my-2">작품 설명 이미지&영상</h2>
         <p className ="my-2 text-gray-600">펀더들이 한눈 프로젝트의 느낌을 알 수 있도록 메인 이미지를 업로드 해주세요.</p>
         <div className ="flex gap-2">
@@ -46,7 +52,7 @@ export default function UploadImageList({imageList, setImageList} :UploadImageLi
 
             <button
                 className ="bg-white px-12 py-16  hover:scale-105" 
-                onClick={onClickAddImageBtn}>
+                onClick={onClickAddVideoBtn}>
                 <Image src={videoUploadButton} alt="nll"/>
             </button>
         </div>
@@ -59,8 +65,18 @@ export default function UploadImageList({imageList, setImageList} :UploadImageLi
                     : null
             }
         </div>
-
-        
+        {
+            isVideoInputModalOpen ?
+        <div className ="fixed left-1/2 bottom-1/2 w-72 h-36 white z-50 bg-white border-2 border-slate-300 ">
+            <p className ="my-3 mx-auto w-fit font-semibold">동영상 UML 링크를 입력해주세요 </p>
+            <input className ="border-2 w-60 mx-auto my-1 border-slate-200 ml-6"/>
+            <div className ="mx-auto w-fit flex gap-6 my-2">
+                <button className ="border-[0.1rem] border-black px-2" onClick ={()=>setIsVideoInputModalOpen(false)}>확인</button>
+                <button className ="border-[0.1rem] border-black px-2" onClick ={()=>setIsVideoInputModalOpen(false)}>취소</button>
+            </div>
+        </div>
+            : null
+        }
     </div>
     )
 }

fcef5d53b5ac7dcf0fbf9cb1f6eda97229f2d4ce
JangGwon <tlsakch510@naver.com>
2024-01-29 02:16:24 +0900

    refactor : 크리에이터 페이지 라우팅 방식 수정

--- pages/createproject.tsx
+++ pages/createproject.tsx
@@ -1,17 +1,26 @@
-import SelectProjectTypeComponent from "@/Components/CreateProject/SelectTypeComponent"
-import CreateNoticeComponent from "@/Components/CreateProject/CreateNoticeComponent"
-import CreateProjectFormComponent from "@/Components/CreateProject/CreateProjectFormComponent"
-import CreateProjectSidebar from "@/Components/CreateProject/CreateProjectSidebar"
-import FundingScheduleForm from "@/Components/CreateProject/FundingScheduleForm/FundingScheduleForm"
-import ProjectDetailForm from "@/Components/CreateProject/ProjectDetailForm/ProjectDetailForm"
-import DevelopNotePriodForm from "@/Components/CreateProject/DevelopNotePriodForm/DevelopNotePriodForm"
-import ProjectRewarodForm from "@/Components/CreateProject/ProjectRewardForm"
+import { useState } from "react";
+
+import SelectProjectTypeComponent from "@/Components/CreateProject/SelectTypeComponent";
+import CreateNoticeComponent from "@/Components/CreateProject/CreateNoticeComponent";
+import CreateProjectFormComponent from "@/Components/CreateProject/CreateProjectFormComponent";
+import CreateProjectSidebar from "@/Components/CreateProject/CreateProjectSidebar";
+import FundingScheduleForm from "@/Components/CreateProject/FundingScheduleForm/FundingScheduleForm";
+import ProjectDetailForm from "@/Components/CreateProject/ProjectDetailForm/ProjectDetailForm";
+import DevelopNotePriodForm from "@/Components/CreateProject/DevelopNotePriodForm/DevelopNotePriodForm";
+import ProjectRewarodForm from "@/Components/CreateProject/ProjectRewardForm";
 
 export default function CreateProject() {
+  const [stage, setStage] = useState("createNot3ice");
   return (
-    <div className ="flex bg-slate-100"> 
-        <CreateProjectSidebar/>
-        <ProjectRewarodForm/>
+    <div className="h-100 bg-gray-100">
+      {stage === "selectProjectType" && <SelectProjectTypeComponent />}
+      {stage === "createNotice" && <CreateNoticeComponent />}
+      {!(stage === "selectProjectType" || stage === "createNotice") && (
+        <div className="flex h-full bg-gray-100">
+          <CreateProjectSidebar />
+          <CreateProjectFormComponent />
+        </div>
+      )}
     </div>
-  )
+  );
 }

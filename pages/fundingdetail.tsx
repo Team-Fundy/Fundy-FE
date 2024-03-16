eff85b46f25970581eabd209cb0f3e4c404f0ba6
HarrySeop <parkpjs3083@naver.com>
2024-03-17 03:13:16 +0900

    feat : 펀딩디테일 페이지 스크롤 구현

---
+++ pages/fundingdetail.tsx
@@ -0,0 +1,14 @@
+export default function fundingDetail() {
+  return (
+    <div className="snap-y snap-mandatory overflow-y-scroll h-[calc(100vh-3rem)]">
+      <div className="snap-start h-[calc(100vh-3rem)] bg-white text-black">
+        {/* 첫 번째 페이지 컴포넌트 내용 */}
+        첫 번째 페이지
+      </div>
+      <div className="snap-start h-[calc(100vh-3rem)] bg-black text-white">
+        {/* 두 번째 페이지 컴포넌트 내용 */}
+        두 번째 페이지
+      </div>
+    </div>
+  );
+}

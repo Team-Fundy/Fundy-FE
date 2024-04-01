b33504ca56220d631c3a5ca2572978e5a1d38db6
HarrySeop <parkpjs3083@naver.com>
2024-04-01 17:44:20 +0900

    Feat: 펀딩 디테일 - 두번째 페이지 레이아웃 구성

--- Components/FundingDetail/FundingDetailSecond.tsx
+++ Components/FundingDetail/FundingDetailSecond.tsx
@@ -1,7 +1,11 @@
+import GridComponent from '@/Components/FundingDetail/GridComponent';
+
 export default function FundingDetaillSecond() { 
   return (
     <div className="flex w-full">
-      <div className="w-3/4 p-4 bg-red-500"></div>
+      <div className="w-3/4 p-4">
+        <GridComponent />
+      </div>
       <div className="w-1/4 p-4 bg-blue-500"></div>
     </div>
   );
